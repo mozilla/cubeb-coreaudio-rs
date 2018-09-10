b567926418955830fc1b052b4c1ea291db66319b
Chun-Min Chang <chun.m.chang@gmail.com>
2018-09-10 16:18:04 -0700

    Rust Interfaces to Cubeb APIs
    Create empty implementations for all the Cubeb Rust interfaces first. Before implementing the underlying platform-dependent code in Rust, it's better to make sureall the Cubeb APIs are callable from C/C++.
    
    On ther other hands, it's a good opportunity to know how the Cubeb Rust interfaces connect to the Cubeb C interfaces. All the Cubeb Rust interfaces are in `cubeb-rs` (https://github.com/djg/cubeb-rs) git repo. The types and APIs of Cubeb are passed from cubeb-sys to cubeb-core to cubeb-backend, and therefore are called from cubeb-backend to cubeb-core to cubeb-ffi. To implement platform-dependent behavior, a `Context` instance is created and is used to call the platform-dependent APIs based on the platform libraries. The above note are commented verbosely in the code for now. They should be removed once it's ready to publish. Just leave them to make other beginners(like me) know how it works for now.

---
+++ src/capi.rs
@@ -0,0 +1,25 @@
+// Copyright © 2018 Mozilla Foundation
+//
+// This program is made available under an ISC-style license.  See the
+// accompanying file LICENSE for details.
+
+use backend::AudioUnitContext;
+// cubeb_backend::{*} is is referred:
+// - capi   : cubeb_backend::capi   (cubeb-core/capi.rs)
+// - ffi    : cubeb_sys::*          (cubeb-core/lib.rs).
+use cubeb_backend::{capi, ffi};
+use std::os::raw::{c_char, c_int};
+
+/// Entry point from C code.
+#[no_mangle]
+pub unsafe extern "C" fn audiounit_rust_init(
+    // `ffi::cubeb` is refered to `pub enum cubeb` (cubeb-sys/context.rs):
+    // cubeb_backend::ffi (cubeb-backend/capi.rs)
+    // -> cubeb_core::ffi (cubeb-core/lib.rs)
+    // -> cubeb_sys::context::cubeb (cubeb-sys/context.rs).
+    c: *mut *mut ffi::cubeb,
+    context_name: *const c_char,
+) -> c_int {
+    // `capi::capi_init` is referred to `cubeb_backend::capi_init`(cubeb-backend/capi.rs).
+    capi::capi_init::<AudioUnitContext>(c, context_name)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright © 2018 Mozilla Foundation
+//
+// This program is made available under an ISC-style license.  See the
+// accompanying file LICENSE for details.
+
+extern crate cubeb_backend;
+
+mod backend;
+mod capi;
+
+pub use capi::audiounit_rust_init;
