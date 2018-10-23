4f12b4c44fe2b5d4f1852af0b6f8d2999cbe162d
Chun-Min Chang <chun.m.chang@gmail.com>
2018-10-23 10:12:24 -0700

    move tests

--- src/backend/owned_critical_section.rs
+++ src/backend/owned_critical_section.rs
@@ -62,106 +62,101 @@ impl Drop for OwnedCriticalSection {
     }
 }
 
-#[cfg(test)]
-mod test_critical_section {
-    use super::*;
+#[test]
+fn test_create_critical_section() {
+    let mut section = OwnedCriticalSection::new();
+    section.init();
+    section.lock();
+    section.assert_current_thread_owns();
+    section.unlock();
+}
 
-    #[test]
-    fn test_create_critical_section() {
-        let mut section = OwnedCriticalSection::new();
-        section.init();
-        section.lock();
-        section.assert_current_thread_owns();
-        section.unlock();
+#[test]
+#[should_panic]
+fn test_critical_section_destroy_without_unlocking_locked() {
+    let mut section = OwnedCriticalSection::new();
+    section.init();
+    section.lock();
+    section.assert_current_thread_owns();
+    // Get EBUSY(16) since we destroy the object
+    // referenced by mutex while it is locked.
+}
+
+#[test]
+#[should_panic]
+fn test_critical_section_unlock_without_locking() {
+    let mut section = OwnedCriticalSection::new();
+    section.init();
+    section.unlock();
+    // Get EPERM(1) since it has no privilege to
+    // perform the operation.
+}
+
+// #[test]
+// #[should_panic]
+// fn test_critical_section_assert_without_locking() {
+//     let mut section = OwnedCriticalSection::new();
+//     section.init();
+//     section.assert_current_thread_owns();
+//     // Get 0 since calling assert_current_thread_owns is equal to
+//     // call lock().
+// }
+
+#[test]
+fn test_critical_section_multithread() {
+    use std::thread;
+    use std::time::Duration;
+
+    struct Resource {
+        value: u32,
+        mutex: OwnedCriticalSection,
     }
 
-    #[test]
-    #[should_panic]
-    fn test_critical_section_destroy_without_unlocking_locked() {
-        let mut section = OwnedCriticalSection::new();
-        section.init();
-        section.lock();
-        section.assert_current_thread_owns();
-        // Get EBUSY(16) since we destroy the object
-        // referenced by mutex while it is locked.
+    let mut resource = Resource {
+        value: 0,
+        mutex: OwnedCriticalSection::new(),
+    };
+
+    resource.mutex.init();
+
+    // Make a vector to hold the children which are spawned.
+    let mut children = vec![];
+
+    // TODO: Only works on 64 bit computer ?
+    println!("resource @ {:p}", &resource);
+    // Rust compiler disallow the pointer to be passed into threads.
+    // A hacky way to do so is to convert the pointer into a value
+    // so it can copy the value to threads.
+    let resource_ptr = &mut resource as *mut Resource as u64;
+
+    for i in 0..10 {
+        // Spin up another thread
+        children.push(thread::spawn(move || {
+            println!("resource address: {:x}", resource_ptr);
+            let res = unsafe {
+                let ptr = resource_ptr as *mut Resource;
+                &mut (*ptr)
+            };
+
+            // Test fails after commenting res.mutex.lock() and
+            // res.mutex.unlock() since the order to run the threads
+            // is random.
+            res.mutex.lock(); // ---------------------------------------+
+                                                                    // |
+            res.value = i;                                           // |
+            thread::sleep(Duration::from_millis(1));                 // | critical
+            println!("this is thread number {}, resource value: {}", // | section
+                    i, res.value);                                  // |
+            // assert_eq!(i, res.value);                             // |
+                                                                    // |
+            res.mutex.unlock(); // <------------------------------------+
+            i == res.value
+        }));
     }
 
-    #[test]
-    #[should_panic]
-    fn test_critical_section_unlock_without_locking() {
-        let mut section = OwnedCriticalSection::new();
-        section.init();
-        section.unlock();
-        // Get EPERM(1) since it has no privilege to
-        // perform the operation.
-    }
-
-    // #[test]
-    // #[should_panic]
-    // fn test_critical_section_assert_without_locking() {
-    //     let mut section = OwnedCriticalSection::new();
-    //     section.init();
-    //     section.assert_current_thread_owns();
-    //     // Get 0 since calling assert_current_thread_owns is equal to
-    //     // call lock().
-    // }
-
-    #[test]
-    fn test_critical_section_multithread() {
-        use std::thread;
-        use std::time::Duration;
-
-        struct Resource {
-            value: u32,
-            mutex: OwnedCriticalSection,
-        }
-
-        let mut resource = Resource {
-            value: 0,
-            mutex: OwnedCriticalSection::new(),
-        };
-
-        resource.mutex.init();
-
-        // Make a vector to hold the children which are spawned.
-        let mut children = vec![];
-
-        // TODO: Only works on 64 bit computer ?
-        println!("resource @ {:p}", &resource);
-        // Rust compiler disallow the pointer to be passed into threads.
-        // A hacky way to do so is to convert the pointer into a value
-        // so it can copy the value to threads.
-        let resource_ptr = &mut resource as *mut Resource as u64;
-
-        for i in 0..10 {
-            // Spin up another thread
-            children.push(thread::spawn(move || {
-                println!("resource address: {:x}", resource_ptr);
-                let res = unsafe {
-                    let ptr = resource_ptr as *mut Resource;
-                    &mut (*ptr)
-                };
-
-                // Test fails after commenting res.mutex.lock() and
-                // res.mutex.unlock() since the order to run the threads
-                // is random.
-                res.mutex.lock(); // ---------------------------------------+
-                                                                         // |
-                res.value = i;                                           // |
-                thread::sleep(Duration::from_millis(1));                 // | critical
-                println!("this is thread number {}, resource value: {}", // | section
-                         i, res.value);                                  // |
-                // assert_eq!(i, res.value);                             // |
-                                                                         // |
-                res.mutex.unlock(); // <------------------------------------+
-                i == res.value
-            }));
-        }
-
-        for child in children {
-            // Wait for the thread to finish. Returns a result.
-            let result = child.join().unwrap();
-            assert!(result)
-        }
+    for child in children {
+        // Wait for the thread to finish. Returns a result.
+        let result = child.join().unwrap();
+        assert!(result)
     }
 }
