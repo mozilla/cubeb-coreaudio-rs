46c8d0bcbc33a085c2be389ba1f7fd7f7f7a7f33
Chun-Min Chang <chun.m.chang@gmail.com>
2019-03-11 17:19:34 -0700

    Move tests operating APIs via cubeb-rs to interfaces.rs and revise them

--- src/backend/tests/interfaces.rs
+++ src/backend/tests/interfaces.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+// Test Templates
+// ------------------------------------------------------------------------------------------------
 fn test_context_operation<F>(name: &'static str, operation: F)
 where
     F: FnOnce(*mut ffi::cubeb),
@@ -16,6 +18,79 @@ where
     unsafe { OPS.destroy.unwrap()(context) }
 }
 
+// Note: The in-out stream initializeed with different device will create an aggregate_device and
+//       result in firing device-collection-changed callbacks. Run in-out streams with tests
+//       capturing device-collection-changed callbacks may cause troubles. See more details in the
+//       comments for test_create_blank_aggregate_device.
+fn test_stream_operation<F>(
+    name: &'static str,
+    input_stream_params: *mut ffi::cubeb_stream_params,
+    output_stream_params: *mut ffi::cubeb_stream_params,
+    data_callback: ffi::cubeb_data_callback,
+    state_callback: ffi::cubeb_state_callback,
+    user_ptr: *mut c_void,
+    operation: F,
+) where
+    F: FnOnce(*mut ffi::cubeb_stream),
+{
+    test_context_operation("context: stream operation", |context_ptr| {
+        let mut stream: *mut ffi::cubeb_stream = ptr::null_mut();
+        let stream_name = CString::new(name).expect("Failed to create stream name");
+        // TODO: stream_init fails when there is no input/output device when the stream parameter
+        //       for input/output is given.
+        assert_eq!(
+            unsafe {
+                OPS.stream_init.unwrap()(
+                    context_ptr,
+                    &mut stream,
+                    stream_name.as_ptr(),
+                    ptr::null(), // Use default input device
+                    input_stream_params,
+                    ptr::null(), // Use default output device
+                    output_stream_params,
+                    4096, // TODO: Get latency by get_min_latency instead ?
+                    data_callback,
+                    state_callback,
+                    user_ptr,
+                )
+            },
+            ffi::CUBEB_OK
+        );
+        assert!(!stream.is_null());
+        operation(stream);
+        unsafe {
+            OPS.stream_destroy.unwrap()(stream);
+        }
+    });
+}
+
+fn test_default_output_stream_operation<F>(name: &'static str, operation: F)
+where
+    F: FnOnce(*mut ffi::cubeb_stream),
+{
+    // Make sure the parameters meet the requirements of AudioUnitContext::stream_init
+    // (in the comments).
+    let mut output_params = ffi::cubeb_stream_params::default();
+    output_params.format = ffi::CUBEB_SAMPLE_FLOAT32NE;
+    output_params.rate = 44100;
+    output_params.channels = 2;
+    output_params.layout = ffi::CUBEB_LAYOUT_UNDEFINED;
+    output_params.prefs = ffi::CUBEB_STREAM_PREF_NONE;
+
+    // TODO: test_stream_operation fails and hit an assertion when there is no device,
+    test_stream_operation(
+        name,
+        ptr::null_mut(),
+        &mut output_params,
+        None,
+        None,
+        ptr::null_mut(),
+        operation,
+    );
+}
+
+// Context Operations
+// ------------------------------------------------------------------------------------------------
 #[test]
 fn test_ops_context_init_and_destroy() {
     test_context_operation("context: init and destroy", |_context_ptr| {});
@@ -407,3 +482,134 @@ fn test_ops_context_register_device_collection_changed_manual() {
         },
     );
 }
+
+// Stream Operations
+// ------------------------------------------------------------------------------------------------
+#[test]
+fn test_ops_stream_init_and_destroy() {
+    test_default_output_stream_operation("stream: init and destroy", |_stream| {});
+}
+
+#[test]
+fn test_ops_stream_start() {
+    test_default_output_stream_operation("stream: start", |stream| {
+        assert_eq!(unsafe { OPS.stream_start.unwrap()(stream) }, ffi::CUBEB_OK);
+    });
+}
+
+#[test]
+fn test_ops_stream_stop() {
+    test_default_output_stream_operation("stream: stop", |stream| {
+        assert_eq!(unsafe { OPS.stream_stop.unwrap()(stream) }, ffi::CUBEB_OK);
+    });
+}
+
+#[test]
+fn test_ops_stream_reset_default_device() {
+    test_default_output_stream_operation("stream: reset default device", |stream| {
+        assert_eq!(
+            unsafe { OPS.stream_reset_default_device.unwrap()(stream) },
+            ffi::CUBEB_ERROR_NOT_SUPPORTED
+        );
+    });
+}
+
+#[test]
+fn test_ops_stream_position() {
+    test_default_output_stream_operation("stream: position", |stream| {
+        let mut position = u64::max_value();
+        assert_eq!(
+            unsafe { OPS.stream_get_position.unwrap()(stream, &mut position) },
+            ffi::CUBEB_OK
+        );
+        assert_eq!(position, 0);
+    });
+}
+
+#[test]
+fn test_ops_stream_latency() {
+    test_default_output_stream_operation("stream: latency", |stream| {
+        let mut latency = u32::max_value();
+        assert_eq!(
+            unsafe { OPS.stream_get_latency.unwrap()(stream, &mut latency) },
+            ffi::CUBEB_OK
+        );
+        assert_ne!(latency, u32::max_value());
+        assert_ne!(latency, 0);
+    });
+}
+
+#[test]
+fn test_ops_stream_set_volume() {
+    test_default_output_stream_operation("stream: set volume", |stream| {
+        assert_eq!(
+            unsafe { OPS.stream_set_volume.unwrap()(stream, 0.5) },
+            ffi::CUBEB_OK
+        );
+    });
+}
+
+#[test]
+fn test_ops_stream_set_panning() {
+    test_default_output_stream_operation("stream: set panning", |stream| {
+        assert_eq!(
+            unsafe { OPS.stream_set_panning.unwrap()(stream, 0.5) },
+            ffi::CUBEB_OK
+        );
+    });
+}
+
+#[test]
+fn test_ops_stream_current_device() {
+    test_default_output_stream_operation("stream: get current device and destroy it", |stream| {
+        let mut device: *mut ffi::cubeb_device = ptr::null_mut();
+        // TODO: stream_get_current_device only returns OK when the machine has both input and
+        //       output devices.
+        assert_eq!(
+            unsafe { OPS.stream_get_current_device.unwrap()(stream, &mut device) },
+            ffi::CUBEB_OK
+        );
+        assert!(!device.is_null());
+        // Uncomment the below to print out the results.
+        // let deviceref = unsafe { DeviceRef::from_ptr(device) };
+        // println!("output: {}", deviceref.output_name().unwrap_or("(no device name)"));
+        // println!("input: {}", deviceref.input_name().unwrap_or("(no device name)"));
+        assert_eq!(
+            unsafe { OPS.stream_device_destroy.unwrap()(stream, device) },
+            ffi::CUBEB_OK
+        );
+    });
+}
+
+#[test]
+fn test_ops_stream_device_destroy() {
+    test_default_output_stream_operation("stream: destroy null device", |stream| {
+        assert_eq!(
+            unsafe {
+                OPS.stream_device_destroy.unwrap()(stream, ptr::null_mut())
+            },
+            ffi::CUBEB_OK // It returns OK anyway.
+        );
+    });
+}
+
+// Enable this after cubeb-rs version is updated to one that implements
+// stream_register_device_changed_callback operation.
+// #[test]
+// fn test_ops_stream_register_device_changed_callback() {
+//     extern "C" fn callback(_: *mut c_void) {}
+
+//     test_default_output_stream_operation("stream: register device changed callback", |stream| {
+//         assert_eq!(
+//             unsafe {
+//                 OPS.stream_register_device_changed_callback.unwrap()(
+//                     stream,
+//                     Some(callback)
+//                 )
+//             },
+//             ffi::CUBEB_OK
+//         );
+//     });
+// }
+
+// TODO: Add a manual test for stream_register_device_changed_callback operation.
