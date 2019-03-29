b91bde44467d22301d15465d7b74158bfbb786bb
Chun-Min Chang <chun.m.chang@gmail.com>
2019-03-29 10:38:22 -0700

    Move AudioUnit related APIs from utils to coreaudio-sys-utils

--- coreaudio-sys-utils/src/lib.rs
+++ coreaudio-sys-utils/src/lib.rs
@@ -1,9 +1,11 @@
 extern crate coreaudio_sys;
 
 pub mod audio_object;
+pub mod audio_unit;
 pub mod string;
 
 pub use audio_object::*;
+pub use audio_unit::*;
 // Re-export coreaudio-sys types
 pub use coreaudio_sys::*;
 pub use string::*;
