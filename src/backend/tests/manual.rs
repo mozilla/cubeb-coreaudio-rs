05d33ae382627297a040de86792106ba339788da
Chun-Min Chang <chun.m.chang@gmail.com>
2019-04-22 11:30:06 -0700

    LGTM changes for test_switch_output_device

--- src/backend/tests/manual.rs
+++ src/backend/tests/manual.rs
@@ -12,11 +12,58 @@ fn test_switch_output_device() {
 
     const SAMPLE_FREQUENCY: u32 = 48_000;
 
+    // Do nothing if there is no 2 available output devices at least.
+    let devices = test_get_devices_in_scope(Scope::Output);
+    if devices.len() < 2 {
+        println!("Need 2 output devices at least.");
+        return;
+    }
+
+    // Make sure the parameters meet the requirements of AudioUnitContext::stream_init
+    // (in the comments).
+    let mut output_params = ffi::cubeb_stream_params::default();
+    output_params.format = ffi::CUBEB_SAMPLE_S16NE;
+    output_params.rate = SAMPLE_FREQUENCY;
+    output_params.channels = 1;
+    output_params.layout = ffi::CUBEB_LAYOUT_MONO;
+    output_params.prefs = ffi::CUBEB_STREAM_PREF_NONE;
+
+    // Used to calculate the tone's wave.
     let mut position: i64 = 0; // TODO: Use Atomic instead.
 
-    fn f32_to_i16_sample(x: f32) -> i16 {
-        (x * f32::from(i16::max_value())) as i16
-    }
+    test_ops_stream_operation(
+        "stream: North American dial tone",
+        ptr::null_mut(), // Use default input device.
+        ptr::null_mut(), // No input parameters.
+        ptr::null_mut(), // Use default output device.
+        &mut output_params,
+        4096, // TODO: Get latency by get_min_latency instead ?
+        Some(data_callback),
+        Some(state_callback),
+        &mut position as *mut i64 as *mut c_void,
+        |stream| {
+            assert_eq!(unsafe { OPS.stream_start.unwrap()(stream) }, ffi::CUBEB_OK);
+            println!("Start playing! Enter 's' to switch device. Enter 'q' to quit.");
+            loop {
+                let mut input = String::new();
+                let _ = io::stdin().read_line(&mut input);
+                assert_eq!(input.pop().unwrap(), '\n');
+                match input.as_str() {
+                    "s" => {
+                        assert!(test_change_default_device(Scope::Output).unwrap());
+                    }
+                    "q" => {
+                        println!("Quit.");
+                        break;
+                    }
+                    x => {
+                        println!("Unknown command: {}", x);
+                    }
+                }
+            }
+            assert_eq!(unsafe { OPS.stream_stop.unwrap()(stream) }, ffi::CUBEB_OK);
+        },
+    );
 
     extern "C" fn state_callback(
         stream: *mut ffi::cubeb_stream,
@@ -58,55 +105,9 @@ fn test_switch_output_device() {
         nframes
     }
 
-    // Do nothing if there is no 2 available output devices at least.
-    let devices = test_get_devices_in_scope(Scope::Output);
-    if devices.len() < 2 {
-        println!("Need 2 output devices at least.");
-        return;
+    fn f32_to_i16_sample(x: f32) -> i16 {
+        (x * f32::from(i16::max_value())) as i16
     }
-
-    // Make sure the parameters meet the requirements of AudioUnitContext::stream_init
-    // (in the comments).
-    let mut output_params = ffi::cubeb_stream_params::default();
-    output_params.format = ffi::CUBEB_SAMPLE_S16NE;
-    output_params.rate = SAMPLE_FREQUENCY;
-    output_params.channels = 1;
-    output_params.layout = ffi::CUBEB_LAYOUT_MONO;
-    output_params.prefs = ffi::CUBEB_STREAM_PREF_NONE;
-
-    test_ops_stream_operation(
-        "stream: North American dial tone",
-        ptr::null_mut(), // Use default input device.
-        ptr::null_mut(), // No input parameters.
-        ptr::null_mut(), // Use default output device.
-        &mut output_params,
-        4096, // TODO: Get latency by get_min_latency instead ?
-        Some(data_callback),
-        Some(state_callback),
-        &mut position as *mut i64 as *mut c_void,
-        |stream| {
-            assert_eq!(unsafe { OPS.stream_start.unwrap()(stream) }, ffi::CUBEB_OK);
-            println!("Start playing! Enter 's' to switch device. Enter 'q' to quit.");
-            loop {
-                let mut input = String::new();
-                let _ = io::stdin().read_line(&mut input);
-                assert_eq!(input.pop().unwrap(), '\n');
-                match input.as_str() {
-                    "s" => {
-                        assert!(test_change_default_device(Scope::Output).unwrap());
-                    }
-                    "q" => {
-                        println!("Quit.");
-                        break;
-                    }
-                    x => {
-                        println!("Unknown command: {}", x);
-                    }
-                }
-            }
-            assert_eq!(unsafe { OPS.stream_stop.unwrap()(stream) }, ffi::CUBEB_OK);
-        },
-    );
 }
 
 #[ignore]
