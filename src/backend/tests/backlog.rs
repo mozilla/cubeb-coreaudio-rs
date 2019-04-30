67d1be86e6fcaea83bc2fe6df001f387ca8c3350
Chun-Min Chang <chun.m.chang@gmail.com>
2019-04-30 10:47:39 -0700

    Avoid poisoning the mutex for device-collection-changed.
    The mutex is considered poisoned whenever a thread panics while holding
    the mutex. Registering a second device-collection-changed callback
    without unregistering the original callback makes the thread panics while
    holding the mutex for the device-collection-changed variables. This
    mutex will be used when the cubeb context is dropped. If the mutex is
    poisoned, we will get another panic when dropping the cubeb context
    because of using a poisoned mutex.

--- src/backend/tests/backlog.rs
+++ src/backend/tests/backlog.rs
@@ -7,56 +7,7 @@ use super::*;
 
 // Interface
 // ============================================================================
-// A panic in `capi_register_device_collection_changed` causes
-// `EXC_BAD_INSTRUCTION` on my MacBook Air but it's fine on my MacBook Pro.
-// It'w weird that it works fine if replacing
-// `register_device_collection_changed: Option<unsafe extern "C" fn(..,) -> c_int>`
-// to `register_device_collection_changed: unsafe extern "C" fn(..,) -> c_int`
-// Test them in `AudioUnitContext` directly instead of calling them via `OPS` for now.
-fn test_context_register_device_collection_changed_twice(devtype: DeviceType) {
-    // We need to initialize the members with type OwnedCriticalSection in
-    // AudioUnitContext, since those OwnedCriticalSection
-    // will be used when register_device_collection_changed is called.
-    let mut ctx = AudioUnitContext::new();
-    ctx.init();
-
-    extern "C" fn callback(_: *mut ffi::cubeb, _: *mut c_void) {}
-
-    assert!(
-        ctx.register_device_collection_changed(
-            devtype,
-            Some(callback),
-            ptr::null_mut()
-        ).is_ok();
-    );
-
-    assert!(
-        ctx.register_device_collection_changed(
-            devtype,
-            Some(callback),
-            ptr::null_mut()
-        ).is_err();
-    );
-}
-
-#[test]
-#[should_panic]
-fn test_context_register_device_collection_changed_twice_input() {
-    test_context_register_device_collection_changed_twice(DeviceType::INPUT);
-}
-
-#[test]
-#[should_panic]
-fn test_context_register_device_collection_changed_twice_output() {
-    test_context_register_device_collection_changed_twice(DeviceType::OUTPUT);
-}
-
-#[test]
-#[should_panic]
-fn test_context_register_device_collection_changed_twice_inout() {
-    test_context_register_device_collection_changed_twice(DeviceType::INPUT | DeviceType::OUTPUT);
-}
-
+// Remove these after test_ops_stream_register_device_changed_callback works.
 #[test]
 fn test_stream_register_device_changed_callback() {
     extern "C" fn callback(_: *mut c_void) {}
