277c47d5d4309767aaa8693aa690f5b30634abfa
Chun-Min Chang <chun.m.chang@gmail.com>
2019-07-25 16:08:18 -0700

    Add tests for device property utils

--- src/backend/tests/api.rs
+++ src/backend/tests/api.rs
@@ -1081,6 +1081,168 @@ fn test_get_device_uid_by_unknwon_device() {
     assert!(get_device_uid(kAudioObjectUnknown, DeviceType::INPUT).is_err());
 }
 
+// get_device_source
+// ------------------------------------
+#[test]
+fn test_get_device_source() {
+    if let Some(device) = test_get_default_device(Scope::Input) {
+        if let Ok(source) = get_device_source(device, DeviceType::INPUT) {
+            println!("{}, {:?}", source, convert_uint32_into_string(source));
+        } else {
+            println!("No data source.");
+        }
+    } else {
+        println!("No input device.");
+    }
+
+    if let Some(device) = test_get_default_device(Scope::Output) {
+        if let Ok(source) = get_device_source(device, DeviceType::OUTPUT) {
+            println!("{}, {:?}", source, convert_uint32_into_string(source));
+        } else {
+            println!("No data source.");
+        }
+    } else {
+        println!("No output device.");
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_get_device_source_by_unknown_device() {
+    assert!(get_device_source(kAudioObjectUnknown, DeviceType::INPUT).is_err());
+}
+
+// get_device_source_name
+// ------------------------------------
+#[test]
+fn test_get_device_source_name() {
+    if let Some(device) = test_get_default_device(Scope::Input) {
+        if let Ok(name) = get_device_source_name(device, DeviceType::INPUT) {
+            println!("{}", name.into_string());
+        } else {
+            println!("No data source name.");
+        }
+    } else {
+        println!("No input device.");
+    }
+
+    if let Some(device) = test_get_default_device(Scope::Output) {
+        if let Ok(name) = get_device_source_name(device, DeviceType::OUTPUT) {
+            println!("{}", name.into_string());
+        } else {
+            println!("No data source name.");
+        }
+    } else {
+        println!("No output device.");
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_get_device_source_name_by_unknown_device() {
+    assert!(get_device_source_name(kAudioObjectUnknown, DeviceType::INPUT).is_err());
+}
+
+// get_device_name
+// ------------------------------------
+#[test]
+fn test_get_device_name() {
+    if let Some(device) = test_get_default_device(Scope::Input) {
+        let name = get_device_name(device, DeviceType::INPUT).unwrap();
+        println!("input device name: {}", name.into_string());
+    } else {
+        println!("No input device.");
+    }
+
+    if let Some(device) = test_get_default_device(Scope::Output) {
+        let name = get_device_name(device, DeviceType::OUTPUT).unwrap();
+        println!("output device name: {}", name.into_string());
+    } else {
+        println!("No output device.");
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_get_device_name_by_unknown_device() {
+    assert!(get_device_name(kAudioObjectUnknown, DeviceType::INPUT).is_err());
+}
+
+// get_device_label
+// ------------------------------------
+#[test]
+fn test_get_device_label() {
+    if let Some(device) = test_get_default_device(Scope::Input) {
+        let name = get_device_label(device, DeviceType::INPUT).unwrap();
+        println!("input device label: {}", name.into_string());
+    } else {
+        println!("No input device.");
+    }
+
+    if let Some(device) = test_get_default_device(Scope::Output) {
+        let name = get_device_label(device, DeviceType::OUTPUT).unwrap();
+        println!("output device label: {}", name.into_string());
+    } else {
+        println!("No output device.");
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_get_device_label_by_unknown_device() {
+    assert!(get_device_label(kAudioObjectUnknown, DeviceType::INPUT).is_err());
+}
+
+// get_device_manufacturer
+// ------------------------------------
+#[test]
+fn test_get_device_manufacturer() {
+    if let Some(device) = test_get_default_device(Scope::Input) {
+        let name = get_device_manufacturer(device, DeviceType::INPUT).unwrap();
+        println!("input device vendor: {}", name.into_string());
+    } else {
+        println!("No input device.");
+    }
+
+    if let Some(device) = test_get_default_device(Scope::Output) {
+        let name = get_device_manufacturer(device, DeviceType::OUTPUT).unwrap();
+        println!("output device vendor: {}", name.into_string());
+    } else {
+        println!("No output device.");
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_get_device_manufacturer_by_unknown_device() {
+    assert!(get_device_manufacturer(kAudioObjectUnknown, DeviceType::INPUT).is_err());
+}
+
+// get_device_buffer_frame_size_range
+// ------------------------------------
+#[test]
+fn test_get_device_buffer_frame_size_range() {
+    if let Some(device) = test_get_default_device(Scope::Input) {
+        let (min, max) = get_device_buffer_frame_size_range(device, DeviceType::INPUT).unwrap();
+        println!("range of input buffer frame size: {}-{}", min, max);
+    } else {
+        println!("No input device.");
+    }
+
+    if let Some(device) = test_get_default_device(Scope::Output) {
+        let (min, max) = get_device_buffer_frame_size_range(device, DeviceType::OUTPUT).unwrap();
+        println!("range of output buffer frame size: {}-{}", min, max);
+    } else {
+        println!("No output device.");
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_get_device_buffer_frame_size_range_by_unknown_device() {
+    assert!(get_device_buffer_frame_size_range(kAudioObjectUnknown, DeviceType::INPUT).is_err());
+}
+
 // AggregateDevice::set_sub_devices
 // ------------------------------------
 #[test]
