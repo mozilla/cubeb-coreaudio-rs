148e57510bddb589b689852f8843be126dc0b675
Chun-Min Chang <chun.m.chang@gmail.com>
2019-11-21 13:53:00 -0800

    Revise stream tester
    To add the command to create or destroy the stream, the cubeb context
    need to be passed to the test function. Instead of using the test
    template that will pass the cubeb stream, using the test template that
    will give te cubeb context is the right way to create the test.

--- src/backend/tests/manual.rs
+++ src/backend/tests/manual.rs
@@ -1,7 +1,6 @@
 use super::utils::{
     test_get_default_device, test_get_default_raw_stream, test_get_devices_in_scope,
-    test_get_stream_with_default_callbacks_by_type, test_ops_stream_operation, Scope, StreamType,
-    TestDeviceSwitcher,
+    test_ops_context_operation, test_ops_stream_operation, Scope, StreamType, TestDeviceSwitcher,
 };
 use super::*;
 use std::io;
@@ -256,70 +255,186 @@ fn test_device_collection_change() {
 #[ignore]
 #[test]
 fn test_stream_tester() {
-    let mut stream_type = StreamType::empty();
-    while stream_type.is_empty() {
-        println!("Select stream type:\n1) Input 2) Output 3) In-Out Duplex 4) Quit");
-        let mut input = String::new();
-        let _ = io::stdin().read_line(&mut input);
-        assert_eq!(input.pop().unwrap(), '\n');
-        stream_type = match input.as_str() {
-            "1" => StreamType::INPUT,
-            "2" => StreamType::OUTPUT,
-            "3" => StreamType::DUPLEX,
-            "4" => {
-                println!("Quit.\n");
-                return;
-            }
-            _ => {
-                println!("Invalid type. Select again.\n");
-                StreamType::empty()
+    test_ops_context_operation("context: stream tester", |context_ptr| {
+        let mut stream_ptr: *mut ffi::cubeb_stream = ptr::null_mut();
+        loop {
+            println!(
+                "commands:\n\
+                 \t'q': quit\n\
+                 \t'c': create a stream\n\
+                 \t'd': destroy a stream\n\
+                 \t's': start the created stream\n\
+                 \t't': stop the created stream"
+            );
+
+            let mut command = String::new();
+            let _ = io::stdin().read_line(&mut command);
+            assert_eq!(command.pop().unwrap(), '\n');
+
+            match command.as_str() {
+                "q" => {
+                    println!("Quit.");
+                    destroy_stream(&mut stream_ptr);
+                    break;
+                }
+                "c" => create_stream(&mut stream_ptr, context_ptr),
+                "d" => destroy_stream(&mut stream_ptr),
+                "s" => start_stream(stream_ptr),
+                "t" => stop_stream(stream_ptr),
+                x => println!("Unknown command: {}", x),
             }
         }
-    }
-    stream_tester_by_type(
-        "loop within a stream",
-        stream_type,
-        None, // Use default input device.
-        None, // Use default output device.
-    );
-}
+    });
 
-fn stream_tester_by_type(
-    name: &'static str,
-    stm_type: StreamType,
-    input_device: Option<AudioObjectID>,
-    output_device: Option<AudioObjectID>,
-) {
-    test_get_stream_with_default_callbacks_by_type(
-        name,
-        stm_type,
-        input_device,
-        output_device,
-        ptr::null_mut(), // Use null since no any callback would be registered.
-        |stream| {
-            println!("Commands:\n\t's' to start stream\n\t't' to stop stream\n\t'q' to to quit.");
-            loop {
-                let mut input = String::new();
-                let _ = io::stdin().read_line(&mut input);
-                assert_eq!(input.pop().unwrap(), '\n');
-                match input.as_str() {
-                    "s" => {
-                        println!("Start stream.");
-                        assert!(stream.start().is_ok());
-                    }
-                    "t" => {
-                        println!("Stop stream.");
-                        assert!(stream.stop().is_ok());
-                    }
-                    "q" => {
-                        println!("Quit.");
-                        break;
-                    }
-                    x => {
-                        println!("Unknown command: {}", x);
-                    }
+    fn start_stream(stream_ptr: *mut ffi::cubeb_stream) {
+        if stream_ptr.is_null() {
+            println!("No stream can start.");
+            return;
+        }
+        assert_eq!(
+            unsafe { OPS.stream_start.unwrap()(stream_ptr) },
+            ffi::CUBEB_OK
+        );
+        println!("Stream started.");
+    }
+
+    fn stop_stream(stream_ptr: *mut ffi::cubeb_stream) {
+        if stream_ptr.is_null() {
+            println!("No stream can stop.");
+            return;
+        }
+        assert_eq!(
+            unsafe { OPS.stream_stop.unwrap()(stream_ptr) },
+            ffi::CUBEB_OK
+        );
+        println!("Stream stopped.");
+    }
+
+    fn destroy_stream(stream_ptr: &mut *mut ffi::cubeb_stream) {
+        if stream_ptr.is_null() {
+            println!("No need to destroy stream.");
+            return;
+        }
+        unsafe {
+            OPS.stream_destroy.unwrap()(*stream_ptr);
+        }
+        *stream_ptr = ptr::null_mut();
+        println!("Stream destroyed.");
+    }
+
+    fn create_stream(stream_ptr: &mut *mut ffi::cubeb_stream, context_ptr: *mut ffi::cubeb) {
+        if !stream_ptr.is_null() {
+            println!("Stream has been created.");
+            return;
+        }
+
+        let mut stream_type = StreamType::empty();
+        while stream_type.is_empty() {
+            println!("Select stream type:\n1) Input 2) Output 3) In-Out Duplex 4) Quit");
+            let mut input = String::new();
+            let _ = io::stdin().read_line(&mut input);
+            assert_eq!(input.pop().unwrap(), '\n');
+            stream_type = match input.as_str() {
+                "1" => StreamType::INPUT,
+                "2" => StreamType::OUTPUT,
+                "3" => StreamType::DUPLEX,
+                "4" => {
+                    println!("Do nothing.");
+                    return;
+                }
+                _ => {
+                    println!("Invalid type. Select again.\n");
+                    StreamType::empty()
                 }
             }
-        },
-    );
+        }
+
+        let mut input_params = get_dummy_stream_params(Scope::Input);
+        let mut output_params = get_dummy_stream_params(Scope::Output);
+
+        let input_stream_params = if stream_type.contains(StreamType::INPUT) {
+            &mut input_params as *mut ffi::cubeb_stream_params
+        } else {
+            ptr::null_mut()
+        };
+        let output_stream_params = if stream_type.contains(StreamType::OUTPUT) {
+            &mut output_params as *mut ffi::cubeb_stream_params
+        } else {
+            ptr::null_mut()
+        };
+
+        let stream_name = CString::new("stream tester").unwrap();
+
+        assert_eq!(
+            unsafe {
+                OPS.stream_init.unwrap()(
+                    context_ptr,
+                    stream_ptr,
+                    stream_name.as_ptr(),
+                    ptr::null_mut(), // default input device
+                    input_stream_params,
+                    ptr::null_mut(), // default output device
+                    output_stream_params,
+                    4096, // latency
+                    Some(data_callback),
+                    Some(state_callback),
+                    ptr::null_mut(), // user pointer
+                )
+            },
+            ffi::CUBEB_OK
+        );
+        assert!(!stream_ptr.is_null());
+
+        extern "C" fn state_callback(
+            stream: *mut ffi::cubeb_stream,
+            _user_ptr: *mut c_void,
+            state: ffi::cubeb_state,
+        ) {
+            assert!(!stream.is_null());
+            assert_ne!(state, ffi::CUBEB_STATE_ERROR);
+        }
+
+        extern "C" fn data_callback(
+            stream: *mut ffi::cubeb_stream,
+            _user_ptr: *mut c_void,
+            _input_buffer: *const c_void,
+            output_buffer: *mut c_void,
+            nframes: i64,
+        ) -> i64 {
+            assert!(!stream.is_null());
+
+            // Feed silence data to output buffer
+            if !output_buffer.is_null() {
+                let stm = unsafe { &mut *(stream as *mut AudioUnitStream) };
+                let channels = stm.core_stream_data.output_stream_params.channels();
+                let samples = nframes as usize * channels as usize;
+                let sample_size =
+                    cubeb_sample_size(stm.core_stream_data.output_stream_params.format());
+                unsafe {
+                    ptr::write_bytes(output_buffer, 0, samples * sample_size);
+                }
+            }
+
+            nframes
+        }
+
+        fn get_dummy_stream_params(scope: Scope) -> ffi::cubeb_stream_params {
+            // The stream format for input and output must be same.
+            const STREAM_FORMAT: u32 = ffi::CUBEB_SAMPLE_FLOAT32NE;
+
+            // Make sure the parameters meet the requirements of AudioUnitContext::stream_init
+            // (in the comments).
+            let mut stream_params = ffi::cubeb_stream_params::default();
+            stream_params.prefs = ffi::CUBEB_STREAM_PREF_NONE;
+            let (format, rate, channels, layout) = match scope {
+                Scope::Input => (STREAM_FORMAT, 48000, 1, ffi::CUBEB_LAYOUT_MONO),
+                Scope::Output => (STREAM_FORMAT, 44100, 2, ffi::CUBEB_LAYOUT_STEREO),
+            };
+            stream_params.format = format;
+            stream_params.rate = rate;
+            stream_params.channels = channels;
+            stream_params.layout = layout;
+            stream_params
+        }
+    }
 }
