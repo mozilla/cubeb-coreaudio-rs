fbad9918655466a37bd879039dd60005fb013ab3
Chun-Min Chang <chun.m.chang@gmail.com>
2020-02-18 15:16:07 -0800

    Add format and lint(clippy) checks in run_tests.sh
    The format check should always be run, in main crate and sub crate, not
    only in Travis CI server only. The format checks and lints(clippy)
    checks should be moved from .travis.yml to run_tests.sh.

--- run_tests.sh
+++ run_tests.sh
@@ -4,11 +4,26 @@ export RUST_BACKTRACE=1
 # Run tests in the sub crate
 # -------------------------------------------------------------------------------------------------
 cd coreaudio-sys-utils
+
+# Format check
+cargo fmt --all -- --check
+
+# Lints check
+cargo clippy -- -D warnings
+
+# Regular Tests
 cargo test
+
 cd ..
 
 # Run tests in the main crate
 # -------------------------------------------------------------------------------------------------
+# Format check
+cargo fmt --all -- --check
+
+# Lints check
+cargo clippy -- -D warnings
+
 # Regular Tests
 cargo test --verbose
 cargo test test_configure_output -- --ignored
