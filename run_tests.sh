01ccaee6872796324688d38ee91be147590af985
Chun-Min Chang <chun.m.chang@gmail.com>
2020-03-24 15:00:05 -0700

    Bail out if 'cargo fmt' fails

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,15 @@ if [[ -z "${RUST_BACKTRACE}" ]]; then
 fi
 echo "RUST_BACKTRACE is set to ${RUST_BACKTRACE}\n"
 
+format_check() {
+  cargo fmt --all -- --check
+  local result=$?
+  if [[ $result -ne 0 ]]; then
+    echo "Please format the code with 'cargo fmt' (version $(cargo fmt -- --version))"
+  fi
+  return $result
+}
+
 # Run tests in the sub crate
 # Run the tests by `cargo * -p <SUB_CRATE>` if it's possible. By doing so, the duplicate compiling
 # between this crate and the <SUB_CRATE> can be saved. The compiling for <SUB_CRATE> can be reused
@@ -17,7 +26,7 @@ SUB_CRATE="coreaudio-sys-utils"
 # `cargo fmt -p *` is only usable in workspaces, so a workaround is to enter to the sub crate
 # and then exit from it.
 cd $SUB_CRATE
-cargo fmt --all -- --check
+format_check || exit $?
 cd ..
 
 # Lints check
@@ -29,7 +38,7 @@ cargo test -p $SUB_CRATE
 # Run tests in the main crate
 # -------------------------------------------------------------------------------------------------
 # Format check
-cargo fmt --all -- --check
+format_check || exit $?
 
 # Lints check
 cargo clippy -- -D warnings
